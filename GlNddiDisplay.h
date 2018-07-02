314d5eb21dd340d2ca78afad6d6ed9d18ebd24ab
Dave Estes <cdestes@cs.unc.edu>
2018-07-02 14:31:55 -0400

    Added SimpleNddiDisplay base class for GlNddiDisplay. Added subregion support to SimulateRender.

--- GlNddiDisplay.h
+++ GlNddiDisplay.h
@@ -7,7 +7,7 @@
 #include <GL/freeglut.h>
 #endif
 
-#include "BaseNddiDisplay.h"
+#include "SimpleNddiDisplay.h"
 
 using namespace nddi;
 
@@ -15,7 +15,7 @@ using namespace nddi;
  * This class adds a public method that returns a reference to the frame buffer
  * so that a GLUT-based application can render it to the window.
  */
-class GlNddiDisplay : public nddi::BaseNddiDisplay {
+class GlNddiDisplay : public SimpleNddiDisplay {
 
 public:
     GlNddiDisplay() {}
@@ -38,27 +38,8 @@ public:
     GLuint GetFrameBufferTex();
     GLuint GetFrameBufferTex(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
 
-    /**
-     * Use to register in bulk all of the rendering cost. This will updates the counts correctly,
-     * but any more detailed information is lost.
-     */
-    Pixel* GetFrameBuffer();
-
-    /**
-     * Triggers a simulated render that only records the cost estimated cost involved.
-     */
-    void SimulateRender();
-
-private:
-    void Render();
-    void Render(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
-    void ComputePixels(unsigned int x, unsigned int y, unsigned int length, bool doCostCalculation);
-    void RegisterBulkRenderCost();
-
-
 protected:
     GLuint    texture_;
-    Pixel    *frameBuffer_;
 };
 
 #endif // GL_NDDI_DISPLAY_H
---
+++ SimpleNddiDisplay.h
@@ -0,0 +1,51 @@
+#ifndef SIMPLE_NDDI_DISPLAY_H
+#define SIMPLE_NDDI_DISPLAY_H
+
+#include "BaseNddiDisplay.h"
+
+using namespace nddi;
+
+/**
+ * This class adds a public method that returns a reference to the frame buffer
+ * so that a GLUT-based application can render it to the window.
+ */
+class SimpleNddiDisplay : public BaseNddiDisplay {
+
+public:
+    SimpleNddiDisplay() {}
+    SimpleNddiDisplay(std::vector<unsigned int> &frameVolumeDimensionalSizes,
+                  unsigned int numCoefficientPlanes, unsigned int inputVectorSize,
+                  bool headless = false,
+                  bool fixed8x8Macroblocks = false, bool useSingleCoeffcientPlane = false);
+    SimpleNddiDisplay(std::vector<unsigned int> &frameVolumeDimensionalSizes,
+                  unsigned int displayWidth, unsigned int displayHeight,
+                  unsigned int numCoefficientPlanes, unsigned int inputVectorSize,
+                  bool headless = false,
+                  bool fixed8x8Macroblocks = false, bool useSingleCoeffcientPlane = false);
+    ~SimpleNddiDisplay();
+
+    /**
+     * Use to register in bulk all of the rendering cost. This will updates the counts correctly,
+     * but any more detailed information is lost.
+     */
+    Pixel* GetFrameBuffer();
+    Pixel* GetFrameBuffer(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
+
+    /**
+     * Triggers a simulated render that only records the cost estimated cost involved.
+     */
+    void SimulateRender();
+    void SimulateRender(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
+
+protected:
+    void Render();
+    void Render(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
+    void ComputePixels(unsigned int x, unsigned int y, unsigned int length, bool doCostCalculation);
+    void RegisterBulkRenderCost(unsigned int sub_x, unsigned int sub_y, unsigned int sub_w, unsigned int sub_h);
+
+
+protected:
+    Pixel    *frameBuffer_;
+};
+
+#endif // SIMPLE_NDDI_DISPLAY_H

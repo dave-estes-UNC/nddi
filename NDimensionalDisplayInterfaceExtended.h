4b38e8155bd29201152f8fa8d356e97371c60d90
Dave Estes <dave.estes@gmail.coml>
2019-01-15 13:00:00 -0500

    Add NDimensionalDisplayInterfaceExtended.h o the doxygen project.

--- NDimensionalDisplayInterfaceExtended.h
+++ NDimensionalDisplayInterfaceExtended.h
@@ -1,9 +1,24 @@
 #ifndef N_DIMENSIONAL_DISPLAY_INTERFACE_EXTENDED_H
 #define N_DIMENSIONAL_DISPLAY_INTERFACE_EXTENDED_H
 
+/**
+ * \file NDimensionalDisplayInterfaceExtended.h
+ *
+ * \brief This file embodies the extensions to the nDDI C++ API.
+ *
+ * This file embodies the extensions to the nDDI C++ API.
+ */
+
+/**
+ * \brief Namespace for the entire nDDI API.
+ *
+ * Namespace for the entire nDDI API.
+ */
 namespace nddi {
     
     /**
+     * \brief This abstract class serves as an extension to the NDDI software interface.
+     *
      * This abstract class serves as an extension to the NDDI software interface.
      */
     class NDimensionalDisplayInterfaceExtended  {
@@ -11,12 +26,16 @@ namespace nddi {
     public:
 
         /**
-         * Copies from one region of the frame volume to another with blending.
+         * \brief Copies from one region of the frame volume to another with blending.
+	 *
+	 * Copies from one region of the frame volume to another with blending.
          *
          * @param start The first pixel in the frame volume to be copied from.
          * @param end The last pixel in the frame volume to be copied from.
          * @param dest The first pixel in the frame volume to be copied to.
-         * @param blend If set, then copy will happy with alpha blending.
+         * @param blend If true, then the copy then the alpha channel for each src
+	 *              pixel will be used to blend the source range over the
+	 *              destination range.
          */
     	virtual void CopyFrameVolume(vector<unsigned int> &start, vector<unsigned int> &end, vector<unsigned int> &dest, bool blend) = 0;
 };
